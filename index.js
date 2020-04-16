de5f8ba379ddb9ebaf19ccfb819b8ea40b128ac4
s10singh97 <s10singh97@yahoo.in>
2020-04-16 23:18:44 +0530

    completed all tasks

--- index.js
+++ index.js
@@ -2,11 +2,56 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
  * @param {Request} request
+ * @param {string} name
  */
+var index;
+const COOKIE_NAME = 'variant'
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const res = await fetch("https://cfw-takehome.developers.workers.dev/api/variants");
+  const res_text = await res.text();
+  const res_json = JSON.parse(res_text);  
+  const cookie = getCookie(request, COOKIE_NAME);
+  let response;
+  if (cookie) {
+    response = await fetch(res_json['variants'][cookie]);
+  }
+  else {
+    index = Math.random() > 0.5 ? 1 : 0;
+    response = await fetch(res_json['variants'][index]);
+    response = new Response(response.body, response);
+    response.headers.set('Set-Cookie', `variant=${index}; Expires=Wed, 21 July 2020 07:28:00 GMT; path=/`)
+  }
+  let response_final = rewriter.transform(response);
+  return response_final;
+}
+
+class ElementHandler
+{
+  element(element) {
+    element.setAttribute('href', 'https://www.cloudflare.com/gaming/');
+    element.setInnerContent('Return to Cloudflare for Gaming');
+  }
+}
+
+const rewriter = new HTMLRewriter()
+  .on('title', {element(element){element.setInnerContent(`Cloudflare Gaming Variant ${index+1}`);}})
+  .on('h1#title', {element(element){element.setInnerContent(`Welcome Cloudflare Gaming Variant ${index+1}`);}})
+  .on('p#description', {element(element){element.setInnerContent('This will redirect you to Cloudflare for Gaming');}})
+  .on('a#url', new ElementHandler())
+
+function getCookie(request, name) {
+  let result = null
+  let cookieString = request.headers.get('Cookie')
+  if (cookieString) {
+    let cookies = cookieString.split(';')
+    cookies.forEach(cookie => {
+      let cookieName = cookie.split('=')[0].trim()
+      if (cookieName === name) {
+        let cookieVal = cookie.split('=')[1]
+        result = cookieVal
+      }
+    })
+  }
+  return result
 }
